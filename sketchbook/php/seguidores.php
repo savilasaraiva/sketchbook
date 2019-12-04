e516134dad15202ebd681cda0d154ae3b663b96e
savilasaraiva <saraivasvfarias@gmail.com>
2019-12-04 19:07:09 -0300

    oiiii

---
+++ sketchbook/php/seguidores.php
@@ -0,0 +1,21 @@
+<?php
+  include 'db.inc.php';
+  $bulk = new MongoDB\Driver\BulkWrite;
+
+  $id = $_GET["id"];
+  $user = $_GET["idUser"];
+  $collection = $col->test;
+  
+  $idUser = new MongoDB\BSON\ObjectId($id);
+
+  try {
+      $bulk->update(
+        array("_id"=>$idUser),
+        array('$addToSet' => array("seguidores" => $user))
+      );
+      $result = $manager->executeBulkWrite($dbname, $bulk);
+      header("Location: ../perfil.php?id=".$id);
+  }catch(MongoDB\Driver\Exception\Exception $e){
+      die("Error Encountered ".$e);
+  }
+?>
